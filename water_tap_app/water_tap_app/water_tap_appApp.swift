161e57ae49f5125d5e9df51d4035a03512fff907
Gary Mejia <gmejima@ucsc.edu>
2024-02-20 15:04:23 -0800

    Started xcode project

---
+++ water_tap_app/water_tap_app/water_tap_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_tap_appApp.swift
+//  water_tap_app
+//
+//  Created by Gary Mejia on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct water_tap_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ water_tap_app/water_tap_appUITests/water_tap_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  water_tap_appUITestsLaunchTests.swift
+//  water_tap_appUITests
+//
+//  Created by Gary Mejia on 2/20/24.
+//
+
+import XCTest
+
+final class water_tap_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
