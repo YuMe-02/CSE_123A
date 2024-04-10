a832da6ee9fab1e50df6dc64aa39b64c3be84bb7
Gary Mejia <gmejiama@ucsc.edu>
2024-04-09 17:16:35 -0700

    commit for starting app

--- water_tap_app/water_tap_app/water_tap_appApp.swift
+++ water_tap_app/water_tap_app/water_tap_appApp.swift
@@ -6,13 +6,13 @@
 //
 
 import SwiftUI
-import HTTPTypes //
+import HTTPTypes //needed for http requests
 //For http stuff
 //https://github.com/apple/swift-http-types
 
 func http_request (method: String, uri: String) -> String {
-    //let request = method + " /" + uri + " HTTP/1.1\r\n\r\n"
-    let request = HTTPRequest(method: .get, scheme: "https", authority: "https://cse123-flowsensor-server.com/", path: "/api/iphone-test")
+    let request = method + " /" + uri + " HTTP/1.1\r\n\r\n"
+    //let request = HTTPRequest(method: .get, scheme: "https", authority: //"https://cse123-flowsensor-server.com/", path: "/api/iphone-test")
     return request
 }
 
