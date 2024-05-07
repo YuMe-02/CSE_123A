553be7459b39f701659596925ba16c1ea2d3cd62
Tim Kraemer <tim022402@gmail.com>
2024-05-07 13:58:38 -0700

    work on front end home view, added http function for sensor registering but havent tested yet

--- water_tap_app/water_tap_app/frontend/HomeFrontend.swift
+++ water_tap_app/water_tap_app/frontend/HomeFrontend.swift
@@ -9,51 +9,65 @@ import Foundation
 import SwiftUI
 
 struct HomeView: View {
-    @State private var responseData1: String = ""
-    @State private var responseData2: String = ""
+    @State private var serialID: String = ""
+    @State private var sinkLocation: String = ""
+    @State private var request_date = Date()
+    @State private var request_sink: String = ""
     @Binding var jwt_token: String
     
+    var greeting: String {
+            let calendar = Calendar.current
+            let hour = calendar.component(.hour, from: Date())
+            
+            switch hour {
+            case 0..<12:
+                return "Good Morning!"
+            case 12..<18:
+                return "Good afternoon!"
+            default:
+                return "Good evening!"
+            }
+        }
+    
     var body: some View {
-        VStack {
-            // Test 1
-            VStack {
-                Text("JWT Token is Below:")
-                Text(jwt_token)
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Test1 of our iPhone API")
-                if responseData1.isEmpty {
-                    Text("Loading...") // Show loading text while fetching data
-                } else {
-                    Text(responseData1.isEmpty ? "Bad request" : responseData1)
+        VStack(alignment: .leading, spacing: 20) {
+            Text(greeting + "👋")
+                .bold()
+                .font(.title)
+                .foregroundColor(.blue)
+                .padding()
+            
+            Section(header: Text("**Register New Sensor**")) {
+                TextField("Serial ID", text: $serialID)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Sink Location", text: $sinkLocation)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Send Request")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
                 }
+                .padding(.horizontal)
+            }
+            Divider()
+            Section(header: Text("**Request Data for Specific Day**")) {
+                DatePicker("Request Date", selection: $request_date, displayedComponents: .date)
+                        .padding()
+                        .datePickerStyle(DefaultDatePickerStyle())
+                TextField("Request Sink", text: $request_sink)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             
-            // Start a new VStack for additional content
-            VStack {
-                Image(systemName: "checkmark")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Test2 of our iPhone API")
-                if responseData2.isEmpty {
-                    Text("Loading...") // Show loading text while fetching data
-                } else {
-                    Text(responseData2.isEmpty ? "Bad request" : responseData2)
-                }
-            }.padding()
+            Spacer()
         }
-        .onAppear {
-            http_get_request_test1 { result in
-                DispatchQueue.main.async {
-                    self.responseData1 = result
-                }
-            }
-            http_get_request_test2(jwt: jwt_token) { result2 in
-                DispatchQueue.main.async {
-                    self.responseData2 = result2
-                }
-            }
-        }
+        .padding()
     }
 }
